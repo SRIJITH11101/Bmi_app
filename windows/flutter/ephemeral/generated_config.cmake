c8019afe59717fc5bcd011b899746ccc44979c08
SRIJITH11101 <dovetonsrijith29@gamil.com>
2023-11-12 15:02:12 +0530

    First commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "H:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "H:\\projects\\prj1\\bmi" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=H:\\flutter"
+  "PROJECT_DIR=H:\\projects\\prj1\\bmi"
+  "FLUTTER_ROOT=H:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=H:\\projects\\prj1\\bmi\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=H:\\projects\\prj1\\bmi"
+  "FLUTTER_TARGET=H:\\projects\\prj1\\bmi\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC84NWFmNWNlZGYwYTdiYmIzNzAyYzc0MmRhNjFkOGRkYjU2YjY3ZTFhLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=H:\\projects\\prj1\\bmi\\.dart_tool\\package_config.json"
+)
